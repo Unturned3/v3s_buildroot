33db85b884422b7848c0346e97b56e8ef1f2d581
Yusen (Richard) Hu <richardyusen@gmail.com>
2021-05-13 16:38:09 +0800

    added curl test to dlt.sh

--- board/licheepi/overlay/root/dlt.sh
+++ board/licheepi/overlay/root/dlt.sh
@@ -2,4 +2,6 @@
 
 # script for testing download speed
 
-time wget -O- mirrors.163.com/debian-cd/10.9.0/amd64/iso-cd/debian-10.9.0-amd64-netinst.iso > /dev/null
+curl -k https://mirrors.163.com/debian-cd/10.9.0/amd64/iso-cd/debian-10.9.0-amd64-netinst.iso > /dev/null
+
+#wget -O- mirrors.163.com/debian-cd/10.9.0/amd64/iso-cd/debian-10.9.0-amd64-netinst.iso > /dev/null
